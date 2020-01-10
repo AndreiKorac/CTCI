cfe1b4065e4fc529896fc34c61259475fc9ab600
Andrei <akorac-777@hotmail.com>
2020-01-09 21:55:24 -0500

    Changed LinkedList from singly to doubly linked, added some tests, ensured all tests still passed throughout

--- DSA/LinkedList.cs
+++ DSA/LinkedList.cs
@@ -5,56 +5,68 @@ namespace DSA
     public class LinkedList
     {
         public Node Head { get; set; }
+        public Node Tail { get; set; }
         public int Count { get; set; }
 
         public void InsertLast(int value)
         {
+            Node newNode = new Node(value);
             if (Head == null)
             {
-                Head = new Node(value);
+                Head = newNode;
+                Tail = newNode;
                 Count++;
             }
             else
             {
-                Node newNode = new Node(value);
-                Node current = Head;
-                while (current.Next != null)
-                {
-                    current = current.Next;
-                }
-                current.Next = newNode;
+                Tail.Next = newNode;
+                newNode.Previous = Tail;
+                Tail = newNode;
                 Count++;
             }
         }
 
         public void InsertAfter(int value, int index)
         {
-            if(index > Count)
+            if(index > Count - 1 || index < 0)
             {
                 throw new IndexOutOfRangeException("Index is out of range of List");
             }
-            Node newNode = new Node(value);
-            Node current = Head;
-            for (int i = 0; i < index - 1; i++)
+            if(index == Count - 1)
             {
-                current = current.Next;
+                InsertLast(value);
             }
-            newNode.Next = current.Next;
-            current.Next = newNode;
-            Count++;
+            else
+            {
+                Node newNode = new Node(value);
+                Node current = Head;
+
+                for (int i = 0; i < index; i++)
+                {
+                    current = current.Next;
+                }
+                newNode.Next = current.Next;
+                newNode.Previous = current;
+                current.Next.Previous = newNode;
+                current.Next = newNode;
+                Count++;
+            }
         }
 
         public void InsertStart(int value)
         {
             if(Head == null)
             {
-                Head = new Node(value);
+                Node newNode = new Node(value);
+                Head = newNode;
+                Tail = newNode;
                 Count++;
             }
             else
             {
                 Node newNode = new Node(value);
                 newNode.Next = Head;
+                Head.Previous = newNode;
                 Head = newNode;
                 Count++;
             }
@@ -69,41 +81,54 @@ namespace DSA
             if(Head.Next == null)
             {
                 Head = null;
+                Tail = null;
                 Count--;
             }
             else
             {
                 Head = Head.Next;
+                Head.Previous = null;
                 Count--;
             }
         }
 
         public void RemoveLast()
         {
-            if (Head == null)
+            if (Tail == null)
             {
                 throw new InvalidOperationException("Cannot remove from an empty list");
             }
             if(Head.Next == null)
             {
                 Head = null;
+                Tail = null;
                 Count--;
             }
             else
             {
-                Node current = Head;
-                while (current.Next.Next != null)
-                {
-                    current = current.Next;
-                }
-                current.Next = null;
+                Tail = Tail.Previous;
+                Tail.Next = null;
                 Count--;
             }
         }
 
-        public void RemoveAtIndex()
+        public void RemoveAtIndex(int index)
         {
-
+            if (index == 0)
+                RemoveFirst();
+            else if (index == Count - 1)
+                RemoveLast();
+            else
+            {
+                Node current = Head;
+                for (int i = 0; i < index; i++)
+                {
+                    current = current.Next;
+                }
+                current.Previous.Next = current.Next;
+                current.Next.Previous = current.Previous;
+                Count--;
+            }
         }
 
         public bool Contains(int value)
@@ -135,6 +160,7 @@ namespace DSA
     public class Node
     {
         public Node Next { get; set; }
+        public Node Previous { get; set; }
         public int Value { get; set; }
 
         public Node(int value)
--- DSATests/LinkedListTests.cs
+++ DSATests/LinkedListTests.cs
@@ -12,6 +12,56 @@ namespace DSATests
         }
 
         [Test]
+        public void InsertLast_EmptyList_InsertsNode()
+        {
+            LinkedList list = new LinkedList();
+
+            list.InsertLast(5);
+
+            Assert.AreEqual(5, list.Tail.Value);
+            Assert.AreEqual(1, list.Count);
+        }
+
+        [Test]
+        public void InsertLast_SeveralInserts_TailCorrect()
+        {
+            LinkedList list = new LinkedList();
+            list.InsertLast(5);
+            list.InsertLast(4);
+            list.InsertStart(13);
+            list.InsertLast(5);
+
+            Assert.AreEqual(5, list.Tail.Value);
+            Assert.AreEqual(4, list.Count);
+        }
+
+        [Test]
+        public void InsertAfter_InsertOutOfRange_ThrowsException()
+        {
+            LinkedList list = new LinkedList();
+
+            Assert.Throws<InvalidOperationException>(() => list.RemoveFirst());
+        }
+
+        [Test]
+        public void InsertAfter_Tail_TailUpdates()
+        {
+            LinkedList list = new LinkedList();
+            list.InsertLast(5);
+            list.InsertLast(4);
+            list.InsertLast(7);
+            list.InsertLast(15);
+            list.InsertAfter(10, 3);
+
+            int valueAtTail = list.Tail.Value;
+            int valueAtLastIndex = list.ValueAt(4);
+
+            Assert.AreEqual(10, valueAtTail);
+            Assert.AreEqual(10, valueAtLastIndex);
+            Assert.AreEqual(5, list.Count);
+        }
+
+        [Test]
         public void InsertStart_InsertsAtStart_HeadValueCorrect()
         {
             LinkedList list = new LinkedList();
@@ -100,6 +150,56 @@ namespace DSATests
 
             Assert.AreEqual(9, valueAtLastNode);
             Assert.AreEqual(3, list.Count);
+        }
+
+        [Test]
+        public void RemoveAtIndex_RemoveFirst_HeadUpdated()
+        {
+            LinkedList list = new LinkedList();
+            list.InsertLast(5);
+            list.InsertLast(4);
+            list.InsertLast(9);
+            list.InsertLast(13);
+
+            list.RemoveAtIndex(0);
+
+            int valueAtUpdatedHead = list.Head.Value;
+
+            Assert.AreEqual(4, valueAtUpdatedHead);
+            Assert.AreEqual(3, list.Count);
+        }
+
+        [Test]
+        public void RemoveAtIndex_RemoveLast_TailUpdated()
+        {
+            LinkedList list = new LinkedList();
+            list.InsertLast(5);
+            list.InsertLast(4);
+            list.InsertLast(9);
+            list.InsertLast(13);
+
+            list.RemoveAtIndex(3);
+
+            int valueAtUpdatedTail = list.Tail.Value;
+
+            Assert.AreEqual(9, valueAtUpdatedTail);
+            Assert.AreEqual(3, list.Count);
+        }
+
+        [Test]
+        public void RemoveAtIndex_RemoveIndex_NodeRemoved()
+        {
+            LinkedList list = new LinkedList();
+            list.InsertLast(5);
+            list.InsertLast(4);
+            list.InsertLast(9);
+            list.InsertLast(13);
+
+            list.RemoveAtIndex(2);
+
+            int expectedValueAtRemovedIndex = list.ValueAt(2);
+
+            Assert.AreEqual(13, expectedValueAtRemovedIndex);
         }
 
         [Test]
