e0b3b9ef5b9b127999f0ebb6956b78918c44730e
AndreiKorac <akorac-777@hotmail.com>
2020-01-19 18:45:12 -0500

    Added SortStack

--- CTCISolutions/ChapterThree.cs
+++ CTCISolutions/ChapterThree.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DSA;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,24 @@ namespace CTCISolutions
 {
     public class ChapterThree
     {
+        public Stack SortStack(Stack input)
+        {
+            Stack tempStack = new Stack();
+
+            while(input.Count > 0)
+            {
+                int temp = input.Pop();
+                while(tempStack.Count > 0 && tempStack.Peek() > temp)
+                {
+                    input.Push(tempStack.Pop());
+                }
+                tempStack.Push(temp);
+            }
+            while(tempStack.Count > 0)
+            {
+                input.Push(tempStack.Pop());
+            }
+            return input;
+        }
     }
 }
--- SolutionsTests/ChapterThreeTests.cs
+++ SolutionsTests/ChapterThreeTests.cs
@@ -18,5 +18,30 @@ namespace SolutionsTests
         {
 
         }
+
+        [Test]
+        public void SortStack_SortsGivenStack()
+        {
+            Stack stack = new Stack();
+            stack.Push(5);
+            stack.Push(3);
+            stack.Push(7);
+            stack.Push(11);
+            stack.Push(4);
+
+            Stack sortedStack = new Stack();
+            sortedStack.Push(11);
+            sortedStack.Push(7);
+            sortedStack.Push(5);
+            sortedStack.Push(4);
+            sortedStack.Push(3);
+
+            stack = _testHelper.SortStack(stack);
+
+            for (int i = 0; i < sortedStack.Count; i++)
+            {
+                Assert.AreEqual(sortedStack.Pop(), stack.Pop());
+            }
+        }
     }
 }
